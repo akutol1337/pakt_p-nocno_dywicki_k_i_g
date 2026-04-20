a25e70a64c3eae758a97f3a650742fc25a94ed89
KBiczak <krzysztofbiczak465@gmail.com>
2026-04-20 13:27:05 +0200

    apdejt

--- Gigapython/zadanieBiblioteka.py
+++ Gigapython/zadanieBiblioteka.py
@@ -19,11 +19,25 @@ class Library:
     def show_books(self):
         for each in self.collection:
             print(each)
+        print('')
 
+    def show_number_of_books(self):
+        print('In the library there are: ' + str(self.number_of_books) + ' books\n')
+def nl():
+    print("")
 library = Library()
 library.add_book(['book1', 'book2', 'book3', 'book4'])
 library.show_books()
-print(library.number_of_books)
+library.show_number_of_books()
 library.borrow_book('book2')
 library.show_books()
-print(library.number_of_books)
+library.show_number_of_books()
+library.add_book(['book5'])
+library.borrow_book('book4')
+library.add_book(['book6', 'book7'])
+library.borrow_book('book1')
+library.borrow_book('book2')
+library.borrow_book('book3')
+library.borrow_book('book5')
+library.show_books()
+library.show_number_of_books()
